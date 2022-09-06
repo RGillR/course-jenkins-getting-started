2719c94f9378fc3a961610d2d0aa7a57ce324fb5
RGillR <liveprocessio@outlook.com>
2022-09-05 23:38:49 -0400

    new jenkins file

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -1,32 +1,53 @@
-pipeline {
+<?xml version='1.1' encoding='UTF-8'?>
+<flow-definition plugin="workflow-job@1207.ve6191ff089f8">
+  <actions>
+    <org.jenkinsci.plugins.pipeline.modeldefinition.actions.DeclarativeJobAction plugin="pipeline-model-definition@2.2114.v2654ca_721309"/>
+    <org.jenkinsci.plugins.pipeline.modeldefinition.actions.DeclarativeJobPropertyTrackerAction plugin="pipeline-model-definition@2.2114.v2654ca_721309">
+      <jobProperties/>
+      <triggers/>
+      <parameters/>
+      <options/>
+    </org.jenkinsci.plugins.pipeline.modeldefinition.actions.DeclarativeJobPropertyTrackerAction>
+  </actions>
+  <description></description>
+  <keepDependencies>false</keepDependencies>
+  <properties/>
+  <definition class="org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition" plugin="workflow-cps@2759.v87459c4eea_ca_">
+    <script>pipeline {
     agent any
-    triggers { pollSCM('* * * * *') }
+
     stages {
-        stage('Checkout') {
+        stage(&apos;checkout&apos;) {
             steps {
-                git url: 'https://github.com/g0t4/jgsu-spring-petclinic.git', branch: 'main'
-            }            
-        }
-        stage('Build') {
-            steps {
-                sh './mvnw clean package'
-                //sh 'false' // true
+                
+                git branch: &apos;main&apos;, url: &apos;https://github.com/RGillR/jgsu-spring-petclinic&apos;
+
             }
-        
+        }    
+        stage(&apos;build&apos;)    {
+            steps {
+                sh &apos;./mvnw clean package&apos;
+                
+        }
+
+            
+
             post {
-                always {
-                    junit '**/target/surefire-reports/TEST-*.xml'
-                    archiveArtifacts 'target/*.jar'
-                }
-                changed {
-                    emailext subject: "Job \'${JOB_NAME}\' (build ${BUILD_NUMBER}) ${currentBuild.result}",
-                        body: "Please go to ${BUILD_URL} and verify the build", 
-                        attachLog: true, 
-                        compressLog: true, 
-                        to: "test@jenkins",
-                        recipientProviders: [upstreamDevelopers(), requestor()]
-                }
+                // If Maven was able to run the tests, even if some of the test
+                // failed, record the test results and archive the jar file.
+               always {
+                   junit &apos;**/target/surefire-reports/TEST-*.xml&apos;
+                   archiveArtifacts &apos;target/*.jar&apos;
+                   }
             }
         }
     }
+    
 }
+
+</script>
+    <sandbox>true</sandbox>
+  </definition>
+  <triggers/>
+  <disabled>false</disabled>
+</flow-definition>
