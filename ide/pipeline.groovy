a4f2a6ce8fb2166813d758ef888cae8dca3a3226
RGillR <liveprocessio@outlook.com>
2022-09-07 15:01:37 -0400

    AGAIN trying JR

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -1,32 +1,17 @@
-<?xml version='1.1' encoding='UTF-8'?>
-<flow-definition plugin="workflow-job@1207.ve6191ff089f8">
-  <actions>
-    <org.jenkinsci.plugins.pipeline.modeldefinition.actions.DeclarativeJobAction plugin="pipeline-model-definition@2.2114.v2654ca_721309"/>
-    <org.jenkinsci.plugins.pipeline.modeldefinition.actions.DeclarativeJobPropertyTrackerAction plugin="pipeline-model-definition@2.2114.v2654ca_721309">
-      <jobProperties/>
-      <triggers/>
-      <parameters/>
-      <options/>
-    </org.jenkinsci.plugins.pipeline.modeldefinition.actions.DeclarativeJobPropertyTrackerAction>
-  </actions>
-  <description></description>
-  <keepDependencies>false</keepDependencies>
-  <properties/>
-  <definition class="org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition" plugin="workflow-cps@2759.v87459c4eea_ca_">
-    <script>pipeline {
+pipeline {
     agent any
 
     stages {
-        stage(&apos;checkout&apos;) {
+        stage('checkout') {
             steps {
                 
-                git branch: &apos;main&apos;, url: &apos;https://github.com/RGillR/jgsu-spring-petclinic&apos;
+                git branch: 'main', url: 'https://github.com/RGillR/jgsu-spring-petclinic'
 
             }
         }    
-        stage(&apos;build&apos;)    {
+        stage('build')    {
             steps {
-                sh &apos;./mvnw clean package&apos;
+                sh './mvnw clean package'
                 
         }
 
@@ -36,8 +21,8 @@
                 // If Maven was able to run the tests, even if some of the test
                 // failed, record the test results and archive the jar file.
                always {
-                   junit &apos;**/target/surefire-reports/TEST-*.xml&apos;
-                   archiveArtifacts &apos;target/*.jar&apos;
+                   junit '**/target/surefire-reports/TEST-*.xml'
+                   archiveArtifacts 'target/*.jar'
                    }
             }
         }
@@ -45,9 +30,3 @@
     
 }
 
-</script>
-    <sandbox>true</sandbox>
-  </definition>
-  <triggers/>
-  <disabled>false</disabled>
-</flow-definition>
